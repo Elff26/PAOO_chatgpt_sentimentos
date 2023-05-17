6f02689efa469ab8d88603d47cb339d83a98c065
Laís Farias - LRI <lais.farias@iteris.com.br>
2023-05-17 17:09:08 -0300

    apostila01(chatgpt-sentimentos): fim da apostila

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
 import os
+import openai
 from dotenv import load_dotenv
 load_dotenv()
-print(os.getenv('OPENAI_API_KEY'))
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+pergunta_padrao = "Qual sentimento da seguinte frase? Responda com apenas uma palavra: Positivo, Negativo ou Neutro. Eis a frase: "
+frase_do_usuario = input("Escreva uma frase para enviarmos para o ChatGPT\n")
+
+prompt = f'{pergunta_padrao}{frase_do_usuario}'
+
+response = openai.Completion.create(
+    model='text-davinci-003',
+    prompt=prompt
+)
+
+print(response.choices[0].text)
+
